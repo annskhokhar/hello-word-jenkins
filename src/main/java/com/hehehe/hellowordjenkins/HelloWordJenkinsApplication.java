0fe3237d71fd1c4a925e130abc8c8b663a1c087e
Anns Shahzad <annskhokhar@gmail.com>
2025-06-25 21:38:20 +0500

    initial commit

---
+++ src/main/java/com/hehehe/hellowordjenkins/HelloWordJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.hehehe.hellowordjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWordJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hehehe/hellowordjenkins/HelloWordJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehehe.hellowordjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
